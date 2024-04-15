7513188d98bcdf65f967d8e6afa1cce6f083b736
Blanca Itzel <itzelgarciaruiz@outlook.com>
2024-04-15 14:51:51 -0600

    Clase -10

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,7 +6,7 @@ module.exports={
     plugins: [
         require("prettier-plugin-astro")
     ],
-    overrides:[
+    overrides: [
         {
             files: "**/*astro",
             options:{
